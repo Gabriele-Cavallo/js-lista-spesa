8d91372e219a67e31bc6ac8bcb0a279deafb5dca
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-03-13 13:33:52 +0100

    feat: add js and html structure to folders ciclo-for and cilo-while

--- ciclo-for/js/script.js
+++ ciclo-for/js/script.js
@@ -10,3 +10,15 @@
 // ];
 // Stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente.
 // Svolgete questo esercizio col ciclo for.
+
+// 1 - Creo la lista da stampare
+const list = [
+    'Latte',
+    'Cacao',
+    'Insalata',
+    'Carote',
+    'Pomodori',
+    'Piatti pronti'
+    ];
+// 2 - Creo la variabile all'elemento del DOM
+// 3 - Stampo ogni elemento dell'array nel DOM con il ciclo for
